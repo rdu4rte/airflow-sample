6745d84d9d172c0f119a88f148aba84265b3c165
rdrt-ezt <raulduarte94@hotmail.com>
2021-07-24 23:40:08 -0300

    chore: etc

---
+++ etc/notes.py
@@ -0,0 +1,12 @@
+project_ideas: dict = {
+    'airflow': '',
+    'cloudflare': '',
+    'typescript': '',
+    'graphql': '',
+    'type-graphql': ''
+}
+
+article_ideas: dict = {
+    'airflow_setup': '',
+    'good_practices_with_ts_graphql': ''
+}
